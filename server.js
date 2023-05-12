c724e2ce3900b799342e9882ccef431b712d5888
Erin Engler <erlacla@msn.com>
2023-05-11 21:35:52 -0700

    adding lesson 4 code

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const app = express();
 const mongodb = require('./db/connect');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./contacts.json');
+
 
 app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(cors())
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
